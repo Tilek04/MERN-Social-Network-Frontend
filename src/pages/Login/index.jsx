b02502f0b942fb761219db32ec7649a4836c52bf
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2023-08-25 22:13:08 +0600

    improve auth page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,11 +3,17 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import { IconButton } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { useForm } from "react-hook-form";
 
 import styles from "./Login.module.scss";
+import { useState } from "react";
 
 export const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -24,6 +30,10 @@ export const Login = () => {
     console.log(values);
   };
 
+  const togglePassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
@@ -33,6 +43,7 @@ export const Login = () => {
         <TextField
           className={styles.field}
           label="E-Mail"
+          type="email"
           error={Boolean(errors.email?.message)}
           helperText={errors.email?.message}
           {...register("email", { required: "Укажите почту" })}
@@ -41,11 +52,15 @@ export const Login = () => {
         <TextField
           className={styles.field}
           label="Пароль"
+          type={showPassword ? '' : 'password'}
           error={Boolean(errors.password?.message)}
           helperText={errors.password?.message}
           {...register("password", { required: "Укажите пороль" })}
           fullWidth
         />
+        <IconButton onClick={togglePassword} size="small">
+          {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+        </IconButton>
         <Button type="submit" size="large" variant="contained" fullWidth>
           Войти
         </Button>
