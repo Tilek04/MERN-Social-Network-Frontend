25e0a098a12ae04d2839f548d3f81948ac52632f
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2023-09-08 20:28:24 +0600

    Validation for registration

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,27 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
+import { useForm } from "react-hook-form";
+import { IconButton } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 import styles from "./Login.module.scss";
+import { selectIsAuth } from "../../redux/slices/auth";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { fetchAuth } from "../../redux/slices/auth";
+import { Navigate } from "react-router-dom";
 
 export const Registration = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      fullName: "Tilek",
+      email: "tilek@gmail.com",
+      password: "qwerty",
+    },
+  });
+
+  // Сохраняем токен в localstorage для авторизации
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+    if (!data.payload) {
+      alert("ERROR");
+    }
+    if ("token" in data.payload) {
+      window.localStorage.setItem("token", data.payload.token);
+
+      console.log(data.payload.token);
+    } else {
+      alert("ERROR");
+    }
+  };
+
+  const togglePassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  if (isAuth === true) {
+    return <Navigate to="/" />;
+  }
+
   return (
-    <Paper classes={{ root: styles.root }}>
-      <Typography classes={{ root: styles.title }} variant="h5">
-        Создание аккаунта
-      </Typography>
-      <div className={styles.avatar}>
-        <Avatar sx={{ width: 100, height: 100 }} />
-      </div>
-      <TextField className={styles.field} label="Полное имя" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
-    </Paper>
+    <form  onSubmit={handleSubmit(onSubmit)}>
+      <Paper classes={{ root: styles.root }}>
+        <Typography classes={{ root: styles.title }} variant="h5">
+          Создание аккаунта
+        </Typography>
+        <div className={styles.avatar}>
+          <Avatar sx={{ width: 100, height: 100 }} />
+        </div>
+        <TextField
+          error={Boolean(errors.fullName?.message)}
+          helperText={errors.fullName?.message}
+          className={styles.field}
+          label="Полное имя"
+          {...register("fullName", { required: "Укажите имя" })}
+          fullWidth
+        />
+        <TextField
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          className={styles.field}
+          label="E-Mail"
+          {...register("email", { required: "Укажте почту" })}
+          fullWidth
+        />
+        <TextField
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          type={showPassword ? "" : "password"}
+          className={styles.field}
+          label="Пароль"
+          {...register("password", { required: "Укажите пороль" })}
+          fullWidth
+        />
+        <IconButton onClick={togglePassword} size="small">
+          {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+        </IconButton>
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Зарегистрироваться
+        </Button>
+      </Paper>
+    </form>
   );
 };
--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,15 +1,14 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from '../../axios'
-import { API } from "../../axios";
+import axios from "../../axios";
 
 
 export const fetchAuth = createAsyncThunk("auth/fetchAuth", async (params) => {
-  const { data } = await axios.post('/auth/login/', params);
+  const { data } = await axios.post("/auth/login/", params);
   return data;
 });
 
 export const fetchAuthMe = createAsyncThunk("auth/fetchAuthMe", async () => {
-  const { data } = await axios.get('/auth/me/');
+  const { data } = await axios.get("/auth/me/");
   return data;
 });
 
